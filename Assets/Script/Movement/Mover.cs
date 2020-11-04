6350960481da37d414249ae4aa52eccde71297a5
Dzmitry Zakharevich <dima.zakharevich95@gmail.com>
2020-11-03 22:33:56 -0700

    Animation matched to movement

--- Assets/Script/Movement/Mover.cs
+++ Assets/Script/Movement/Mover.cs
@@ -5,14 +5,13 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Transform target;
-
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
     private void MoveToCursor()
     {
@@ -24,5 +23,13 @@ public class Mover : MonoBehaviour
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
     }
-        
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+
 }
